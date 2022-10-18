77201c7bfdee6a7cf162a0bcb46d64f8480ff56c
Sobhit Sharma <sobhit.sharma@kaleyra.com>
2022-10-18 13:22:10 +0530

    Loops and if

---
+++ IfCases.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10
+c = 7
+
+if c < a:
+    print("c is smaller than a")
+
+elif c > b:
+    print("c is higher than b")
+
+elif c > a and c < b:
+    print("c is between a and b")
---
+++ WhileLoop.py
@@ -0,0 +1,4 @@
+numberOfTrees = 0
+while numberOfTrees < 5:
+    numberOfTrees += 1
+    print("I am having so much fun!")
